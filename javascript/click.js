bcd07cbf6183e6778993652bd6017d6150fe35c5
kairulla <kayrulla@bk.ru>
2021-11-12 10:51:21 +0600

    наладил работу тега iframe

--- javascript/click.js
+++ javascript/click.js
@@ -1,11 +1,1 @@
-window.onload = function () { //дожидаемся полной загрузки страницы    
-    var a = document.getElementById('switch'); //получаем идентификатор элемента
-    a.onclick = function () { //вешаем на него событие
-        //производим какие-то действия
-        if (this.innerHTML == 'On') this.innerHTML = 'Off';
-        else this.innerHTML = 'On'; //предотвращаем переход по ссылке href
-        return false;
-    }
-}
-
-alert("Добро пожаловать на мой сайт! Спасибо что зашли.");
+# alert("Добро пожаловать на мой сайт! Спасибо что зашли.");
